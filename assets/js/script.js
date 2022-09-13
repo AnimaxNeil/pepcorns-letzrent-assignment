f754932012e6cee2f82ae266bf9cd6892b7b00f1
AnimaxNeil <animaxneil@gmail.com>
2022-09-14 00:36:12 +0530

    js working now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,37 @@
-// // basic interactions script
+// basic interactions script
 
-// const pitch_tab_btn = document.getElementById("tab-pitch");
-// const detail_tab_btn = document.getElementById("tab-details");
-// const updates_tab_btn = document.getElementById("updates-pitch");
-// const comments_tab_btn = document.getElementById("comments-pitch");
+const pitch_tab_btn = document.getElementById("tab-pitch");
+const detail_tab_btn = document.getElementById("tab-details");
+// const updates_tab_btn = document.getElementById("tab-updates");
+// const comments_tab_btn = document.getElementById("tab-comments");
 
-// const pitch_tab_content = document.getElementById("tab-pitch-content");
-// const detail_tab_content = document.getElementById("tab-details-content");
-// const updates_tab_content = document.getElementById("updates-pitch-content");
-// const comments_tab_content = document.getElementById("comments-pitch-content");
+const pitch_tab_content = document.getElementById("tab-pitch-content");
+const detail_tab_content = document.getElementById("tab-details-content");
+// const updates_tab_content = document.getElementById("tab-updates-content");
+// const comments_tab_content = document.getElementById("tab-comments-content");
 
-// const tab_unselect_class = ".tab-unselected";
-// const tab_content_unselect_class = ".tab-content-unselected";
+const tab_unselect_class = "tab-unselected";
+const tab_content_unselect_class = "tab-content-unselected";
 
-// const tab_btns = [pit_tab_btn, detail_tab_btn, updates_tab_btn, comments_tab_btn];
-// const tab_contents = [pitch_tab_btn, detail_tab_content, updates_tab_content, comments_tab_content];
+const tab_btns = [pitch_tab_btn, detail_tab_btn];
+const tab_contents = [pitch_tab_content, detail_tab_content];
 
-// const change_tab = (selected_tab_index) => {
-//     for (tb in tab_btns) {
-//         if (!tb.classList.contains(tab_unselect_class)) tb.classList.add(tab_unselect_class);
-//     }
-//     for (tc in tab_contents) {
-//         if (!tc.classList.contains(tab_content_unselect_class)) tc.classList.add(tab_content_unselect_class);
-//     }
-//     if (selected_tab_index) {
-//         tab_btns[selected_tab_index].classList.remove(tab_content_unselect_class);
-//         tab_contents[selected_tab_index].classList.remove(tab_content_unselect_class);
-//     }
-// };
+const change_tab = (index) => {
+    let i;
+    for (i = 0; i < tab_btns.length; i++) {
+        if (!tab_btns[i].classList.contains(tab_unselect_class)) tab_btns[i].classList.add(tab_unselect_class);
+    }
+    for (i = 0; i < tab_contents.length; i++) {
+        if (!tab_contents[i].classList.contains(tab_content_unselect_class)) tab_contents[i].classList.add(tab_content_unselect_class);
+    }
+    tab_btns[index].classList.remove(tab_unselect_class);
+    tab_contents[index].classList.remove(tab_content_unselect_class);
+};
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     for (btn in tab_btns) {
-//         btn.addEventListener("click", change_tab);
-//     }
-//     for (btn in tab_btns) {
-//         change_tab(tab_btns[0]);
-//     }
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    for (let i = 0; i < tab_btns.length; i++) {
+        tab_btns[i].addEventListener("click", () => change_tab(i));
+    }
+    change_tab(0);
+});
 
